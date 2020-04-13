9ac01f76df16210a378625698a9fea3fe33a02d8
Dantara <supercreed2@gmail.com>
2020-04-14 00:54:55 +0300

    Enrollment continue

--- lib/attendance_app/accounts.ex
+++ lib/attendance_app/accounts.ex
@@ -3,6 +3,9 @@ defmodule AttendanceApp.Accounts do
   alias AttendanceApp.Repo
 
   alias AttendanceApp.Accounts.Role
+  alias AttendanceApp.Accounts.Enrollment
+  alias AttendanceApp.Attendance.Class
+  alias AttendanceApp.Accounts.User
 
   def list_roles do
     Repo.all(Role)
@@ -28,5 +31,38 @@ defmodule AttendanceApp.Accounts do
 
   def change_role(%Role{} = role) do
     Role.changeset(role, %{})
+  end
+
+  def user_enrolls(user) do
+    # %{classes: enrolls} =
+    #   user
+    user_enrolls_query =
+      from u in User,
+      where: u.id == ^user.id,
+      join: c in assoc(u, :classes),
+      select: %{id: c.id, title: c.title},
+      order_by: [{:asc, c.title}]
+      # |> Repo.preload(:classes)
+
+    # enrolls
+    Repo.all(user_enrolls_query)
+  end
+
+  def user_not_enrolls(user) do
+    user_enrolls_query =
+      from u in User,
+      where: u.id == ^user.id,
+      join: c in assoc(u, :classes),
+      select: %{id: c.id, title: c.title}
+
+    all_classes =
+      from c in Class,
+      select: %{id: c.id, title: c.title}
+
+    sorted = except(all_classes, ^user_enrolls_query)
+      from s in subquery(sorted),
+      order_by: [{:asc, :title}]
+
+    Repo.all(sorted)
   end
 end
--- lib/attendance_app_web/controllers/student_controller.ex
+++ lib/attendance_app_web/controllers/student_controller.ex
@@ -1,11 +1,17 @@
 defmodule AttendanceAppWeb.StudentController do
   use AttendanceAppWeb, :controller
+  alias AttendanceApp.Accounts
 
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
   def enroll(conn, _params) do
-    render(conn, "enroll.html")
+    current_user = Pow.Plug.current_user(conn)
+
+    enrolls = Accounts.user_enrolls(current_user)
+    not_enrolls = Accounts.user_not_enrolls(current_user)
+
+    render(conn, "enroll.html", enrolls: enrolls, not_enrolls: not_enrolls)
   end
 end
