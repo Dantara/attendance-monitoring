7acbc27c4ac509c8f603eb797841dd3950d348ea
Dantara <supercreed2@gmail.com>
2020-04-18 21:26:34 +0300

    Prod fix 2

--- config/prod.exs
+++ config/prod.exs
@@ -9,7 +9,8 @@ config :attendance_app, AttendanceAppWeb.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/cache_manifest.json",
-  server: true
+  server: true,
+  check_origin: ["attendance_app-inno.herokuapp.com"]
 
 config :logger, level: :info
 
